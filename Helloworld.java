2bc36d65eccedbb9cbc5fa6385d75530f6231b4a
Abhishek Pateriya <abhishekpateria77@gmail.com>
2023-07-31 08:06:53 +0530

    updated code

--- Helloworld.java
+++ Helloworld.java
@@ -2,5 +2,6 @@ package gitexample;
 class Helloworld{
     public static void main(String[] args) {
        System.out.println("hello abhishek,how are you?");
+       System.out.println("hi abhishek,how are you?");
     }
 } 
